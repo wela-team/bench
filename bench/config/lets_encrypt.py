c75a5fdb9856d1a811f6bdc12810b957439a82be
Elman Torres <63641794+wela-elman@users.noreply.github.com>
2024-05-30 14:07:05 +0800

    Update lets_encrypt.py

--- bench/config/lets_encrypt.py
+++ bench/config/lets_encrypt.py
@@ -58,7 +58,8 @@ def run_certbot_and_setup_ssl(site, custom_domain, bench_path, interactive=True)
 
 	try:
 		interactive = '' if interactive else '-n'
-		exec_cmd("{path} {interactive} certonly --standalone -d {site}".format(path=get_certbot_path(), interactive=interactive, site=custom_domain or site))
+		exec_cmd("{path} {interactive} --config /etc/letsencrypt/configs/{site}.cfg certonly".format(path=get_certbot_path(), interactive=interactive, site=custom_domain or site))
+		# exec_cmd("{path} {interactive} certonly --standalone -d {site}".format(path=get_certbot_path(), interactive=interactive, site=custom_domain or site))
 	except CommandFailedError:
 		service('nginx', 'start')
 		print("There was a problem trying to setup SSL for your site")
