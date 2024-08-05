760ed0f67fbb1fa208d9ae998c59162697e8aa4c
wela-elman <elman.torres@wela.online>
2024-08-05 13:41:15 +0800

    fix default

--- playbooks/install.py
+++ playbooks/install.py
@@ -140,7 +140,7 @@ def install_prerequisites():
 	install_package('pip3', 'python3-pip')
 
 	success = run_os_command({
-		'python3': "sudo -H python3 -m pip install --upgrade setuptools cryptography==2.8 ansible==2.8.5 pip"
+		'python3': "sudo -H python3 -m pip install --upgrade setuptools pyOpenSSL cryptography==2.8 ansible==2.8.5 pip"
 	})
 
 	if not (success or shutil.which('ansible')):
